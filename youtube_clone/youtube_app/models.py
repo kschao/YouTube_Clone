8a7e8bc28c09c3886be37721488b35007d7dc2a8
Steve Clark <steve.a.clark48@gmail.com>
2021-08-18 16:03:27 -0500

    created class channel

--- youtube_clone/youtube_app/models.py
+++ youtube_clone/youtube_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.expressions import F
 
 # Create your models here.
 class Comment(models.Model):
@@ -16,4 +17,9 @@ class Video(models.Model):
     description = models.CharField(max_length=300)
     path = models.CharField(max_length=70)
     dattime = models.DateTimeField(auto_now=True, blank=False, null=False)
+    user = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    
+class Channel(models.Model):
+    channel_name = models.CharField(max_length=50, blank=False, null=False)
+    subscribers = models.IntegerField(default=0, blank=False, null=False)
     user = models.ForeignKey('auth.user', on_delete=models.CASCADE)
