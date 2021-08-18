30fccaaec662462ca646e446ff619de5902a2e1e
Steve Clark <steve.a.clark48@gmail.com>
2021-08-18 12:01:36 -0500

    deleted database and security key

--- youtube_clone/youtube_clone/settings.py
+++ youtube_clone/youtube_clone/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lbx(m73c&4k$kwvbcup_nnu5in8qxrbyh%4&-@n0fq^vmbx8%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,14 +73,6 @@ WSGI_APPLICATION = 'youtube_clone.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
