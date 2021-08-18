5fa21fb82fce06a2f3454c0e94bbb4ab99f8b97b
Steve Clark <steve.a.clark48@gmail.com>
2021-08-18 11:43:27 -0500

    added defaults to database

--- youtube_clone/youtube_clone/local_settings.py
+++ youtube_clone/youtube_clone/local_settings.py
@@ -3,7 +3,14 @@ SECRET_KEY = 'django-insecure-lbx(m73c&4k$kwvbcup_nnu5in8qxrbyh%4&-@n0fq^vmbx8%s
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'youtube_clone_database',
+        'USER': 'root',
+        'PASSWORD': '197^ADc198@SAc',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
     }
 }
