757c65873851221b51ed2bdd5fa84746a09a79fd
Steve Clark <steve.a.clark48@gmail.com>
2021-08-18 11:39:18 -0500

    copied secret_key and database

---
+++ youtube_clone/youtube_clone/local_settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'django-insecure-lbx(m73c&4k$kwvbcup_nnu5in8qxrbyh%4&-@n0fq^vmbx8%s'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
