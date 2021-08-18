480450d75ff9d659b9b71192fa6c0d62df3a5dbb
Steve Clark <steve.a.clark48@gmail.com>
2021-08-18 16:21:51 -0500

    Created Class VideoPage(APIView)

--- youtube_clone/youtube_app/views.py
+++ youtube_clone/youtube_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Comment, Reply, Video, Channel
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class VideoPage(APIView):
+    
+    def get(self, request):
+        video = Video.objects.all()
+        serializer = VideoSerializer(video, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = VideoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
