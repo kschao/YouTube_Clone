3f8694ea7e8f602fa4142a2287925e57baa907e0
Steve Clark <steve.a.clark48@gmail.com>
2021-08-19 09:38:16 -0500

    updated url paths

--- youtube_clone/youtube_app/urls.py
+++ youtube_clone/youtube_app/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comments/', views.CommentList.as_view()),
-    path('comments-id/<str:video_id>/', views.CommentDetail.as_view()),
-    path('comments/<int:pk>/<str:video_id>/', views.CommentLike.as_view()),
-    path('comments/<str:video_id>/<int:pk>/', views.CommentDislike.as_view()),
-    path('comments-reply/', views.ReplyList.as_view()),
-    path('comments-reply/<int:comment>/', views.ReplyDetail.as_view()),
+    path('video/', views.VideoPage.as_view()),
+    path('comments/', views.CommentSection.as_view()),
+    path('reply/', views.ReplySection.as_view()),
+    path('channel/', views.views.ChannelSection.as_view()),
 ]
