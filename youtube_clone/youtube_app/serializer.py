8b360c7878d6ac58da960dcb9b3f6054d2603326
Steve Clark <steve.a.clark48@gmail.com>
2021-08-19 08:19:08 -0500

    Added Video Serializer

--- youtube_clone/youtube_app/serializer.py
+++ youtube_clone/youtube_app/serializer.py
@@ -1,6 +1,6 @@
 from django.db import models
 from rest_framework import serializers
-from .models import Comment, Reply
+from .models import Comment, Reply, Video
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,9 @@ class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
         fields = ['comment', 'body']
+        
+class VideoSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Video
+        fields = ['id', 'youtube_id', 'title', 'like_video', 'dislike_video']
